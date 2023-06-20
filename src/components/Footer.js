cf20a179f2da86d99c2774aa03c2f7d3f1db063a
victec1737 <victora.hernandez@espoch.edu.ec>
2023-06-19 23:09:29 -0500

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p> &copy; 2023 vic._.tec@hotmail.com </p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Logo from "../assets/logo2.png";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="leftSide">
+        <img src={Logo} alt="Logo" />
+        <div className="hiddenLinks">
+          <Link to="/"> Home </Link>
+          <Link to="/menu"> Menu </Link>
+          <Link to="/about"> About </Link>
+          <Link to="/music"> Music </Link>
+        </div>
+      </div>
+      <div className="rightSide">
+        <Link to="/"> Home </Link>
+        <Link to="/menu"> Menu </Link>
+        <Link to="/about"> About </Link>
+        <Link to="/music"> Music </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
